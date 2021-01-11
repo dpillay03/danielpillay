463a469d54944f524884b93b11d72239ec22fc89
dpillay03 <dpillay03@gmail.com>
2021-01-10 17:49:56 -1000

    Updates

---
+++ precache-manifest.5311d59f6c2d97d599d86a1fdda3617a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "507e1cdb07c1a3aacf2b5d570536be72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b12e9e87b88d92e5332",
+    "url": "/static/css/2.48c3d4a3.chunk.css"
+  },
+  {
+    "revision": "85694eeab3c2d442774c",
+    "url": "/static/css/main.066c16d5.chunk.css"
+  },
+  {
+    "revision": "2b12e9e87b88d92e5332",
+    "url": "/static/js/2.a726e0ff.chunk.js"
+  },
+  {
+    "revision": "85694eeab3c2d442774c",
+    "url": "/static/js/main.41901376.chunk.js"
+  },
+  {
+    "revision": "3772c879b3eb7df4d74f",
+    "url": "/static/js/runtime~main.f933d74f.js"
+  },
+  {
+    "revision": "8ca6aa5f893b450037116fe0ebb2cb94",
+    "url": "/static/media/combat-capitol.8ca6aa5f.md"
+  },
+  {
+    "revision": "5ad8d98baec349647c665d4b7eb399d9",
+    "url": "/static/media/deception-pass.5ad8d98b.md"
+  },
+  {
+    "revision": "ab906175abc24b7db8b1d8c1f34da0bb",
+    "url": "/static/media/hawaii-teleport.ab906175.md"
+  },
+  {
+    "revision": "dfd8fc3005327b129bbdfb46bc7204e9",
+    "url": "/static/media/heeia-bay.dfd8fc30.md"
+  },
+  {
+    "revision": "f1732d9f55e9ac3e356e1ffbf4f18538",
+    "url": "/static/media/kona-golden-hour.f1732d9f.md"
+  },
+  {
+    "revision": "dceb0a4458a87b2595d0c35ae1d4b552",
+    "url": "/static/media/poetry-in-motion.dceb0a44.md"
+  },
+  {
+    "revision": "a14f08325d487561f0943808a1a5faa2",
+    "url": "/static/media/skydive.a14f0832.md"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
